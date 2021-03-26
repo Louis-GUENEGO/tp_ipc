1a7d57fda652545d741d33dd216fac0d02e74e54
Louis GUENEGO <louis.guenego@gmail.com>
2021-03-26 10:42:41 +0100

    added rdv.c

--- fifo1.c
+++ fifo1.c
@@ -6,8 +6,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-
 int main(void){
 
   char out;
--- rdv.c
+++ rdv.c
@@ -2,18 +2,21 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/wait.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include "dijkstra.h"
 
 #define CLE 1000
 
-main() {
+int main(void) {
   int pid;
   int semid;
 
-  semid = sem_create(CLE, ...);
+  semid = sem_create(CLE, 0);
 
 
   pid=fork();
@@ -23,18 +26,18 @@ main() {
     exit(-1);
   }
   else if(pid == 0) { // Fils
-    sleep(5) ;
+    sleep(3) ;
     printf("FILS: V(sem)\n");
-    ...
-      exit(0);
+    V(semid);
+    exit(0);
   }
   else {	// Pere
     printf("PERE: P(sem)\n");
-    ...
-      printf("PERE: RDV avec mon fils\n");
-
-      sem_delete(...);
-      exit(0);
+    P(semid);
+    printf("PERE: RDV avec mon fils\n");
+    sem_delete(semid);
+    wait(0);
+    printf("PERE: mon fils est mort\n");
+    exit(0);
   }
 }
-
